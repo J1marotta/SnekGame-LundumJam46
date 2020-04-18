df316a36ba394edbc6e15f1a8e6cd8fe460984f6
J1marotta <james.marotta@outlook.com>
2020-04-19 09:53:39 +1000

    update dangers every 2 warnings

--- src/main.js
+++ src/main.js
@@ -49,26 +49,32 @@ const updateHeroFrames = () => {
 }
  
 const moveHero = () => {
+  
+
   if (btn.right){
     hero.x += hero.speed;
     hero.flipH = false;
     hero.image = updateHeroFrames()
+    score++
   }
     
   if (btn.left)  {
     hero.x -= hero.speed;
     hero.flipH = true;
     hero.image = updateHeroFrames()  
+    score++
   }
 
   if (btn.up)  {  
     hero.y -= hero.speed; 
     hero.image = updateHeroFrames()
+    score++
   }
 
   if (btn.down){ 
     hero.y += hero.speed;
     hero.image = updateHeroFrames()
+    score++ 
   }
 
   if (hero.x < playArea.Xmin) hero.x = playArea.Xmin;
@@ -81,54 +87,86 @@ const moveHero = () => {
 }
 
 
-function getRandomIntInclusive(min, max) {
+function getRandomIntInclusive(min, max, x) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 8) + min)
+
+    let buffer = x ? 8 : 16
+
+    let num = Math.floor(Math.random() * (max - min +  buffer) + min) 
+     if(x && num < playArea.Xmin) num = playArea.Xmin
+     if(x && num > playArea.Xmax) num = playArea.Xmax
+     if(num < playArea.Ymin) num = playArea.Ymin
+     if(num > playArea.Ymax) num = playArea.Ymax
+
+    return num
 }
 
-const tiles = []
+const warnings = []
 let timeLastbox = 0;
 
-const createBox = (tiles) => {
-  
-  
+const createBox = (warnings) => {
   const tile = {
-    x: getRandomIntInclusive(playArea.Xmin, playArea.Xmax),
+    x: getRandomIntInclusive(playArea.Xmin, playArea.Xmax, true),
     y: getRandomIntInclusive(playArea.Ymin, playArea.Ymax)
   }
 
-  const dt = (Date.now() - timeLastbox) / 1000  >= 5 
-
-  if (dt ){ 
-    tiles.push(tile)
+  const warning = (Date.now() - timeLastbox) / 1000  >= 5 
+  
+  if (warning) { 
+    warnings.push(tile)
+    startDanger()
     timeLastbox = Date.now()
-   }  
-  return tiles 
+   }
+  return warnings 
 
 }
+
+
+
+let dangers = []
+let counter = 0
+const startDanger = () => {
+    counter ++ 
+
+  if(counter > 2) {
+    dangers.push(warnings.shift())
+    counter = 0
+  }
+
+  return dangers
+}
+
 
 const background = getMap("map");
 paper(7);
 pen(0)
 
 let score = 0;
-let previousTime = Date.now();
-
 
 exports.update = function () {  
   moveHero()
   cls();
   draw(background, 0, 8);
   print('SNEK', 8, 1)
-  print(score++, 140, 1)
+  print(score, 140, 1)
   camera(0,0);
   sprite(hero.image, hero.x, hero.y, hero.flipH, );
-  createBox(tiles)
+  createBox(warnings)
 
-  tiles.map( t => 
+
+
+  warnings.map( t =>  
+      sprite(58, t.x, t.y)
+  )
+  
+  
+  dangers.map( t => 
     sprite(230, t.x, t.y)  
   )
+
+  // checkContact(hero, dangers)
+  
 
 
 };
