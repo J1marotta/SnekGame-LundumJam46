78aa174af40e4c7fbc762c8d841e61dfea0bcf54
J1marotta <james.marotta@outlook.com>
2020-04-18 21:12:23 +1000

    spawns explosions

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ const MAP_LIMITS = {
 
 const Lx = MAP_LIMITS.x * TILE_WIDTH
 const Ly = MAP_LIMITS.y * TILE_HEIGHT
+
 const playArea = {
   Xmin: 8,
   Ymin: 16,
@@ -24,12 +25,7 @@ const hero = {
   width: TILE_WIDTH,
   flipH: false,
   image:  153,
-
 };
-
-const background = getMap("map");
-paper(7);
-pen(0)
 
 
 
@@ -75,24 +71,64 @@ const moveHero = () => {
     hero.image = updateHeroFrames()
   }
 
-  if(hero.x < playArea.Xmin) hero.x = playArea.Xmin;
-  if(hero.x > playArea.Xmax) hero.x = playArea.Xmax;
-  if(hero.y < playArea.Ymin ) hero.y = playArea.Ymin;
-  if(hero.y > playArea.Ymax) hero.y = playArea.Ymax;
- 
+  if (hero.x < playArea.Xmin) hero.x = playArea.Xmin;
+  if (hero.x > playArea.Xmax) hero.x = playArea.Xmax;
+  if (hero.y < playArea.Ymin ) hero.y = playArea.Ymin;
+  if (hero.y > playArea.Ymax) hero.y = playArea.Ymax;
   
   
+
 }
 
 
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 8) + min)
+}
+
+const tiles = []
+let timeLastbox = 0;
+
+const createBox = (tiles) => {
+  
+  
+  const tile = {
+    x: getRandomIntInclusive(playArea.Xmin, playArea.Xmax),
+    y: getRandomIntInclusive(playArea.Ymin, playArea.Ymax)
+  }
+
+  const dt = (Date.now() - timeLastbox) / 1000  >= 5 
+
+  if (dt ){ 
+    tiles.push(tile)
+    timeLastbox = Date.now()
+   }  
+  return tiles 
+
+}
+
+const background = getMap("map");
+paper(7);
+pen(0)
+
+let score = 0;
+let previousTime = Date.now();
 
 
 exports.update = function () {  
-  
   moveHero()
   cls();
   draw(background, 0, 8);
-  print('SNEK', 0, 1)  
+  print('SNEK', 8, 1)
+  print(score++, 140, 1)
   camera(0,0);
   sprite(hero.image, hero.x, hero.y, hero.flipH, );
+  createBox(tiles)
+
+  tiles.map( t => 
+    sprite(230, t.x, t.y)  
+  )
+
+
 };
