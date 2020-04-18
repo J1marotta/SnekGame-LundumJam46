19dd90af93269991fe93acbf5dee43c96e171699
J1marotta <james.marotta@outlook.com>
2020-04-18 16:16:42 +1000

    better frames and run animation

--- src/main.js
+++ src/main.js
@@ -10,51 +10,63 @@ const MAP_LIMITS = {
 const Lx = MAP_LIMITS.x * TILE_WIDTH
 const Ly = MAP_LIMITS.y * TILE_HEIGHT
 const playArea = {
-  min: 8,
-  max: 144
+  Xmin: 8,
+  Ymin: 16,
+  Xmax: 144,
+  Ymax: 152,
 }
 
 
 const hero = {
   x: 0,
   y: 0,
-  speed: 2,
+  speed: 1,
   width: TILE_WIDTH,
   flipH: false,
-  image:  Math.random() < 0.5 ? 153 : 154
+  image:  153,
+
 };
 
 const background = getMap("map");
 paper(7);
 
+const updateHeroFrames = () => Math.random() > 0.5 ? 153 : 154
  
-const moveHero = () =>  {
+const moveHero = () => {
   if (btn.right){
     hero.x += hero.speed;
     hero.flipH = false;
+    hero.image = updateHeroFrames()
   }
     
   if (btn.left)  {
     hero.x -= hero.speed;
     hero.flipH = true;
+    hero.image = updateHeroFrames()  
   }
 
-  if (btn.up)    hero.y -= hero.speed;
-  if (btn.down)  hero.y += hero.speed;
+  if (btn.up)  {  
+    hero.y -= hero.speed; 
+    hero.image = updateHeroFrames()
+  }
 
-  if(hero.x < playArea.min) hero.x = playArea.min;
-  if(hero.x > playArea.max) hero.x = playArea.max;
-  if(hero.y < playArea.min ) hero.y = playArea.min;
-  if(hero.y > playArea.max) hero.y = playArea.max;
+  if (btn.down){ 
+    hero.y += hero.speed;
+    hero.image = updateHeroFrames()
+  }
+
+  if(hero.x < playArea.Xmin) hero.x = playArea.Xmin;
+  if(hero.x > playArea.Xmax) hero.x = playArea.Xmax;
+  if(hero.y < playArea.Ymin ) hero.y = playArea.Ymin;
+  if(hero.y > playArea.Ymax) hero.y = playArea.Ymax;
 }
-
 
 
 
 exports.update = function () {  
   moveHero()
   cls();
-  draw(background, 0, 0);
+  draw(background, 0, 8);
   camera(0,0);
-  sprite(Math.random() < 0.5 ? 153 : 154, hero.x, hero.y, hero.flipH, );
+  sprite(hero.image, hero.x, hero.y, hero.flipH, );
 };
