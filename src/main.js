3b0d8f7465bf36c95c3ccafcfbeef618533e1fe6
J1marotta <james.marotta@outlook.com>
2020-04-18 14:53:11 +1000

    basic movement successful

--- src/main.js
+++ src/main.js
@@ -7,38 +7,54 @@ const MAP_LIMITS = {
   y: 160,
 }
 
+const Lx = MAP_LIMITS.x * TILE_WIDTH
+const Ly = MAP_LIMITS.y * TILE_HEIGHT
+const playArea = {
+  min: 8,
+  max: 144
+}
+
+
 const hero = {
   x: 0,
   y: 0,
-  flipH: false,
+  speed: 2,
   width: TILE_WIDTH,
+  flipH: false,
+  image:  Math.random() < 0.5 ? 153 : 154
 };
 
 const background = getMap("map");
 paper(7);
 
+ 
+const moveHero = () =>  {
+  if (btn.right){
+    hero.x += hero.speed;
+    hero.flipH = false;
+  }
+    
+  if (btn.left)  {
+    hero.x -= hero.speed;
+    hero.flipH = true;
+  }
+
+  if (btn.up)    hero.y -= hero.speed;
+  if (btn.down)  hero.y += hero.speed;
+
+  if(hero.x < playArea.min) hero.x = playArea.min;
+  if(hero.x > playArea.max) hero.x = playArea.max;
+  if(hero.y < playArea.min ) hero.y = playArea.min;
+  if(hero.y > playArea.max) hero.y = playArea.max;
+}
 
 
-// // const move = (hero, btn, Lx, Ly ) => {
-//   if (btn.right && hero.x < Lx ) hero.x += 1;
-//   if (btn.left && hero.x > 0 ) hero.x -= 1;
-//   if (btn.up && hero.y < Ly)    hero.y -= 1;
-//   if (btn.down && hero.y > 0)  hero.y += 1;
 
-// // }
-
-const Lx = MAP_LIMITS.x * TILE_WIDTH
-const Ly = MAP_LIMITS.y * TILE_HEIGHT
 
 exports.update = function () {  
-  
-  if (btn.right) hero.x += 1;
-  if (btn.left)  hero.x -= 1;
-  if (btn.up)    hero.y -= 1;
-  if (btn.down)  hero.y += 1;
-
+  moveHero()
   cls();
   draw(background, 0, 0);
   camera(0,0);
-  sprite(153, hero.x, hero.y, hero.x < 0, );
+  sprite(Math.random() < 0.5 ? 153 : 154, hero.x, hero.y, hero.flipH, );
 };
