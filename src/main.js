e063e34fcb08881df288c3549e638166dc4383af
J1marotta <james.marotta@outlook.com>
2020-04-18 14:00:38 +1000

    basic movement

--- src/main.js
+++ src/main.js
@@ -1,22 +1,44 @@
-var character = {
-  x: 60,
-  y: 60
+
+let TILE_WIDTH  = settings.tileSize.width  || settings.tileSize[0];
+let TILE_HEIGHT = settings.tileSize.height || settings.tileSize[1];
+
+const MAP_LIMITS = {
+  x: 160,
+  y: 160,
+}
+
+const hero = {
+  x: 0,
+  y: 0,
+  flipH: false,
+  width: TILE_WIDTH,
 };
 
-var background = getMap("map");
+const background = getMap("map");
 paper(7);
 
 
 
-exports.update = function () {
-  console.log(JSON.stringify(btn))
-  if (btn.right) character.x += 1;
-  if (btn.left)  character.x -= 1;
-  if (btn.up)    character.y -= 1;
-  if (btn.down)  character.y += 1;
+// // const move = (hero, btn, Lx, Ly ) => {
+//   if (btn.right && hero.x < Lx ) hero.x += 1;
+//   if (btn.left && hero.x > 0 ) hero.x -= 1;
+//   if (btn.up && hero.y < Ly)    hero.y -= 1;
+//   if (btn.down && hero.y > 0)  hero.y += 1;
+
+// // }
+
+const Lx = MAP_LIMITS.x * TILE_WIDTH
+const Ly = MAP_LIMITS.y * TILE_HEIGHT
+
+exports.update = function () {  
+  
+  if (btn.right) hero.x += 1;
+  if (btn.left)  hero.x -= 1;
+  if (btn.up)    hero.y -= 1;
+  if (btn.down)  hero.y += 1;
 
   cls();
   draw(background, 0, 0);
-  camera(character.x , character.y)
-  sprite(153, character.x + 50, character.y + 50);
+  camera(0,0);
+  sprite(153, hero.x, hero.y, hero.x < 0, );
 };
